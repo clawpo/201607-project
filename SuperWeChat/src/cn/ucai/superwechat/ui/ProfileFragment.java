4ef0b90bd5e5068a19eb7b0f0b810446fc7d488e
耿小泰 <clawpo@foxmail.com>
2016-09-23 17:21:23 +0800

    个人中心fragment设置昵称和帐号名

--- SuperWeChat/src/cn/ucai/superwechat/ui/ProfileFragment.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -1,19 +1,51 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.SuperWeChatHelper;
+import cn.ucai.superwechat.bean.UserAvatar;
 
 /**
  * Created by clawpo on 2016/9/23.
  */
 public class ProfileFragment extends Fragment {
+    @BindView(R.id.profile_image)
+    ImageView mProfileImage;
+    @BindView(R.id.tvname)
+    TextView mTvname;
+    @BindView(R.id.tvmsg)
+    TextView mTvmsg;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setUserInfo();
+    }
+
+    private void setUserInfo() {
+        UserAvatar user = SuperWeChatHelper.getInstance().getCurrentUserAvatar();
+        EaseUserUtils.setUserNick(user.getMUserNick(),mTvname);
+        EaseUserUtils.setUserNick("微信号:"+user.getMUserName(),mTvmsg);
+        EaseUserUtils.setUserAvatar(SuperWeChatApplication.applicationContext,user.getMUserName(),mProfileImage);
     }
 }
