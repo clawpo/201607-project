7adad87e821ed436684420b5befa4a94529c8fcb
耿小泰 <clawpo@foxmail.com>
2016-10-08 15:33:43 +0800

    从通讯录选择好友聊天后,按返回键直接返回到mainactivity

--- SuperWeChat/src/cn/ucai/superwechat/ui/ChatActivity.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/ChatActivity.java
@@ -54,10 +54,10 @@ public class ChatActivity extends BaseActivity{
     @Override
     public void onBackPressed() {
         chatFragment.onBackPressed();
-        if (EasyUtils.isSingleActivity(this)) {
+//        if (EasyUtils.isSingleActivity(this)) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
-        }
+//        }
     }
     
     public String getToChatUsername(){
